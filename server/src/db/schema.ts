59ddf4e4f4b87fc87c9f429a49743213215d44ad
Aqib Ansari <aqibansari72a@gmail.com>
2026-02-06 04:30:26 +0530

    feat: update users table schema to use UUID for id and add createdAt timestamp

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,7 +1,8 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { pgTable, varchar, uuid, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid().primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  createdAt: timestamp().notNull().defaultNow(),
 });
